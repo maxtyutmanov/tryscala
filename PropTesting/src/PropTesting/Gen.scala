27b1a00b7e62049ebbe7469e0cfe4dc6cfb961c1
maxtyutmanov <tyutmanovmd@gmail.com>
2020-03-09 20:06:48 +0300

    union, weighted unit for Gen

--- PropTesting/src/PropTesting/Gen.scala
+++ PropTesting/src/PropTesting/Gen.scala
@@ -4,15 +4,34 @@ case class Gen[A](sample: State[RNG,A]) {
   def map[B](f: A => B): Gen[B] =
     Gen(sample.map(f))
     
+  def flatMap[B](f: A => Gen[B]): Gen[B] = 
+    Gen(sample.flatMap(a => f(a).sample))
+    
   def generate(rng: RNG): (A, RNG) =
     sample.run(rng)
 }
 
 object Gen {
+  def join[A](gg: Gen[Gen[A]]): Gen[A] =
+    gg.flatMap(ga => ga)
+    
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    boolean.flatMap(b => if (b) g1 else g2)
+    
+  def weighted[A](g1: (Gen[A],Double), g2: (Gen[A],Double)): Gen[A] = {
+    val scaledW1 = g1._2 / (g1._2 + g2._2)
+    Gen(Random.double).flatMap(d => if (d < scaledW1) g1._1 else g2._1)
+  }
+    
+  def map2[A,B,C](ga: Gen[A], gb: Gen[B])(f: (A, B) => C): Gen[C] = for {
+    a <- ga
+    b <- gb
+  } yield f(a, b)
+  
   def choose(start: Int, stopExclusive: Int): Gen[Int] =
     Gen(Random.inRange(start, stopExclusive))
-    
-  def randStr(len: Int, alphabet: String): Gen[String] = {
+  
+  def randStr(len: Gen[Int], alphabet: String): Gen[String] = {
     val charGen = choose(0, alphabet.length).map(ix => alphabet(ix))
     listOfN(len, charGen).map(charlist => charlist.mkString(""))
   }
@@ -36,4 +55,7 @@ object Gen {
     
   def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
     Gen(Random.sequence(List.fill(n)(g.sample)))
+    
+  def listOfN[A](n: Gen[Int], g: Gen[A]): Gen[List[A]] =
+    n.flatMap(nval => listOfN(nval, g))
 }
