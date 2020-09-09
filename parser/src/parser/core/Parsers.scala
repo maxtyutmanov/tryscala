1db29ba0922f435abdac2fdd21f9f7f19e7e3906
maxtyutmanov <tyutmanovmd@gmail.com>
2020-09-09 22:32:40 +0300

    parser combinators lib

---
+++ parser/src/parser/core/Parsers.scala
@@ -0,0 +1,17 @@
+package parser.core
+
+trait Parsers[ParseError, Parser[+_]] {	self =>
+  def run[A](p: Parser[A])(input: String): Either[ParseError,A]
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+  implicit def char(c: Char): Parser[Char]
+  implicit def string(s: String): Parser[String]
+  implicit def operators[A](p: Parser[A]) = ParserOps[A](p)
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]):
+    ParserOps[String] = ParserOps(f(a))
+  def listOfN[A](n: Int, p: Parser[A]): Parser[List[A]]
+  
+  case class ParserOps[A](p: Parser[A]) {
+    def |[B>:A](p2: Parser[B]): Parser[B] = self.or(p,p2)	
+    def or[B>:A](p2: Parser[B]): Parser[B] = self.or(p,p2)
+  }
+}
---
+++ parser/src/parser/core/package.scala
@@ -0,0 +1,6 @@
+package parser
+
+
+package object core {
+  
+}
